0ed399c3dcaec6950bdd2f1b17ebc82f8f985228
David Hollis <david@davidhollis.net>
2023-06-18 11:11:06 -0700

    Add connection pool options to the config (#11)
    TSIA. The `config.json` changes probably read a bit better with
    whitespace changes turned off.
    
    Closes #3

--- src/application.rs
+++ src/application.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use diesel::pg::PgConnection;
 use diesel::r2d2::{Pool, ConnectionManager};
 
@@ -11,13 +13,24 @@ pub struct Application {
 
 impl Application {
     pub fn from_config(config: Config) -> anyhow::Result<Application> {
-        let connection_manager = ConnectionManager::<PgConnection>::new(config.database_url.clone());
+        let connection_manager = ConnectionManager::<PgConnection>::new(config.database.url.clone());
 
-        // TODO: make some of the connection pool options (e.g., max pool size, expiration, etc.)
-        //       configurable through `config`
-        let connection_pool =
+        let mut connection_pool_builder =
             Pool::builder()
-                .build(connection_manager)?;
+                .min_idle(config.database.connection_pool.min_idle_count)
+                .max_lifetime(config.database.connection_pool.max_lifetime_ms.map(Duration::from_millis))
+                .idle_timeout(config.database.connection_pool.idle_timeout_ms.map(Duration::from_millis))
+                .test_on_check_out(config.database.connection_pool.test_connection_on_checkout);
+        
+        if let Some(max_size) = config.database.connection_pool.max_size {
+            connection_pool_builder = connection_pool_builder.max_size(max_size);
+        }
+
+        if let Some(connection_timeout_ms) = config.database.connection_pool.connection_timeout_ms {
+            connection_pool_builder = connection_pool_builder.connection_timeout(Duration::from_millis(connection_timeout_ms));
+        }
+
+        let connection_pool = connection_pool_builder.build(connection_manager)?;
         
         Ok(Application {
             config,
--- src/config.rs
+++ src/config.rs
@@ -2,10 +2,27 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize)]
 pub struct Config {
-    #[serde(default = "default_database_url")]
-    pub database_url: String,
+    pub database: Database,
     pub bind: Bind,
     pub log: log4rs::config::RawConfig,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Database {
+    #[serde(default = "default_database_url")]
+    pub url: String,
+    pub connection_pool: ConnectionPool,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ConnectionPool {
+    pub max_size: Option<u32>,
+    pub min_idle_count: Option<u32>,
+    pub max_lifetime_ms: Option<u64>,
+    pub idle_timeout_ms: Option<u64>,
+    pub connection_timeout_ms: Option<u64>,
+    #[serde(default = "default_test_on_checkout")]
+    pub test_connection_on_checkout: bool,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -22,4 +39,8 @@ pub enum Bind {
 
 fn default_database_url() -> String {
     std::env::var("DATABASE_URL").expect("No database URL found in config or environment")
+}
+
+fn default_test_on_checkout() -> bool {
+    true
 }
